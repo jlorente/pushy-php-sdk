c5540db035a5d0892e37d99660c6299bddba0694
Rayner <rayner@homedoctor.es>
2021-11-11 10:30:17 +0100

    [Fix] Default class name in exception handler

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -107,7 +107,7 @@ class Handler
         } elseif (array_key_exists($statusCode, $this->exceptionsByStatusCode)) {
             $class = $this->exceptionsByStatusCode[$statusCode];
         } else {
-            $class = 'Pushy';
+            $class = 'PushyApi';
         }
 
         $class = "\\Jlorente\\Pushy\\Exception\\{$class}Exception";
